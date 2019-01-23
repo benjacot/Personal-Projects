4d7c5ede94ace4ed2a5c6ea2831bdfab5a298032
benjacot <jacotb@wwu.edu>
2019-01-23 11:07:48 -0800

    Added Animal class, gave it a global name, a constructor, and 2 functions

---
+++ Android Dev/Episode6/Animal.java
@@ -0,0 +1,15 @@
+class Animal {
+  String name;
+
+  public Animal (String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int numOfLegs() {
+    return 0;
+  }
+}
